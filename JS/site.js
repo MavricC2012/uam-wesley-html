be82ec6545b55fe83cbdad12c5b2c7a826b8e8ba
Craig Weysham <battlemaster2012@gmail.com>
2015-11-30 07:02:52 -0600

    Working with google maps

--- JS/site.js
+++ JS/site.js
@@ -1,1 +1,33 @@
 //site.js//
+var marker;
+
+function toggleBounce() {
+    "use strict";
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
+
+function initMap() {
+    "use strict";
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: {
+            lat: 33.5883,
+            lng: -91.8131
+        }
+    });
+
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: {
+            lat: 33.5883,
+            lng: -91.8131
+        }
+    });
+    marker.addListener('click', toggleBounce);
+}
